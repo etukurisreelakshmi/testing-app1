bc313f0854d70e2e5c61a25ebb5f356dd05641f1
bharath.k@newage-global.com <bharath.k@newage-global.com>
2021-04-26 00:12:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+ constructor(props){
+   super(props);
+   this.state={date:new Date()};
+ }
+ tick=()=>{
+   this.setState({date:new Date()});
+ };
+ componentDidMount(){
+   this.timerId=setInterval(
+       ()=>this.tick(),2000)
+ }
+componentWillUnmount(){
+   clearInterval(this.tick());
 }
-
+render(){
+  return(
+      <div>
+        <h1>Hello, world</h1>
+        <h2>it is{this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+  )
+}
+}
 export default App;
+//<h2>It is {new Date().toLocaleTimeString()}.</h2>
